c0a69337cce4f231d6c4ac48cb4cc24a81a6d8bd
Satyamall <thesatyamall123@gmail.com>
2022-03-22 13:15:00 +0530

    Satya, Backend deployment

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+app.get("/",function(req,res){
+    res.send("Hello World!");
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
